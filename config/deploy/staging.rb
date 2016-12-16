e941ee1f400955c4243c81aace35be8cbe853a54
rohan2609 <routrohan90@gmail.com>
2016-12-16 16:09:42 +0530

    for_deployment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -14,7 +14,7 @@ role :db,  %w(deploy@example.com)
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w(web app), my_property: :my_value
+server '54.212.205.24', user: 'deploy', roles: %w(web app), my_property: :my_value
 
 # Custom SSH Options
 # ==================
