e651b591766be901cf1ded4906b77043ba87a9e2
rohan2609 <routrohan90@gmail.com>
2016-12-16 12:51:41 +0530

    update_all_datepicker

--- vendor/assets/javascripts/vendor.js
+++ vendor/assets/javascripts/vendor.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui/datepicker
+//= require jquery-ui
 //= require dataTables/jquery.dataTables
 //= require moment
 //= require_tree .
