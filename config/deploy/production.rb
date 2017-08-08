e1d3621078e9db155ccd893d8a218fc99b4ce9f3
Dhanshree Kamble <dhanshree.j.kamble@gmail.com>
2017-08-08 07:05:46 +0000

    production.rb edited online with Bitbucket

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '52.88.165.146', user: 'ubuntu', roles: %w{app db web}
+server '34.212.215.227', user: 'ubuntu', roles: %w{app db web}
 
 # Custom SSH Options
 # ==================
