65432637f8ac503b9399c3f10949617b24eb00ea
Manoj Kumar <manojk.ror@gmail.com>
2017-08-27 12:34:25 +0530

    change ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '34.212.215.227', user: 'ubuntu', roles: %w{app db web}
+server '52.35.102.153', user: 'ubuntu', roles: %w{app db web}
 
 # Custom SSH Options
 # ==================
