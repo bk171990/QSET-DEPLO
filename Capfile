eb6effec4b0b0eda07212dee7d3e5ece9e486810
Manoj Kumar <manojk.ror@gmail.com>
2017-08-27 14:19:34 +0530

    added few more change

--- Capfile
+++ Capfile
@@ -25,10 +25,10 @@ install_plugin Capistrano::SCM::Git
 require 'capistrano/bundler'
 require 'capistrano/rails/assets'
 require 'capistrano/rails/migrations'
-require 'capistrano/nginx'
+# require 'capistrano/nginx'
 require 'capistrano/puma'
 require 'capistrano/puma/nginx'
-require 'capistrano/upload-config'
+# require 'capistrano/upload-config'
 
 # Load custom tasks from `lib/capistrano/tasks' if you have any defined
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,6 @@
 # # config valid only for current version of Capistrano
 # lock '3.8.1'
 
-server '52.35.102.153', port: 80, roles: [:web, :app, :db], primary: true
 set :application, 'school'
 
 set :repo_url, 'https://manoj-qset:Manoj87!@bitbucket.org/DhanshreeK/school.git'
@@ -67,49 +66,4 @@ namespace :deploy do
     end
   end
 
-end
-
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
-
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
-
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
 end
